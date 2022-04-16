fa78da26fed7768f12f90160c804cfceb2c5f6b7
blackcoffepls <69984284+blackcoffepls@users.noreply.github.com>
2022-04-16 01:10:48 -0300

    .

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -18,21 +19,21 @@ func dbvar() string {
 	}
 
 	// CONFIG VARS
-	/*DB_HOST := os.Getenv("DB_HOST")
+	DB_HOST := os.Getenv("DB_HOST")
 	DB_PORT := os.Getenv("DB_PORT")
 	DB_PASSWORD := os.Getenv("DB_PASSWORD")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_USER := os.Getenv("DB_USER")
 
 	dsn := ("host=" + DB_HOST +
-	" user=" + DB_USER +
-	" password=" + DB_PASSWORD +
-	" dbname=" + DB_NAME +
-	" port=" + DB_PORT +
-	" sslmode=disable TimeZone=Asia/Shanghai")*/
+		" user=" + DB_USER +
+		" password=" + DB_PASSWORD +
+		" dbname=" + DB_NAME +
+		" port=" + DB_PORT +
+		" sslmode=disable TimeZone=Asia/Shanghai")
 
 	// LOCAL DB
-	dsn := "host=localhost user=root password=secret dbname=root port=5431 sslmode=disable TimeZone=Asia/Shanghai"
+	//dsn := "host=localhost user=root password=secret dbname=root port=5431 sslmode=disable TimeZone=Asia/Shanghai"
 
 	return dsn
 }
